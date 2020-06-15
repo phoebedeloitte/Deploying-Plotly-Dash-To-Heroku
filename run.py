69726a40f0d9f0dbd56fdbed24d0dcde44e7dfda
phoebedeloitte <66926555+phoebedeloitte@users.noreply.github.com>
2020-06-15 10:55:50 +1000

    Update run.py

--- run.py
+++ run.py
@@ -10,30 +10,16 @@ server.secret_key = os.environ.get('secret_key', 'secret')
 app = dash.Dash(name = __name__, server = server)
 app.config.supress_callback_exceptions = True
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-    dcc.Graph(
-        id='example-graph',    
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    ),
-    dcc.Input(id='my-id', value='initial value', type="text"),
-    html.Div(id='my-div')
-])
+app.layout=html.Div(children=[html.H1(children='Sample Dash Web App Dashboard'),
+                              html.Div(children=''''Dash: A web based app to show Bar Graph'''),
+                                      dcc.Graph(id='dash_graph', figure = {'data':[{'x':[1,2,3,4,5],'y':[4,6,3,8,1],'type':'bar','name':'Bread'},
+                                                                                    {'x':[1,2,3,4,5],'y':[9,10,11,1],'type':'bar','name':'Bread'},
+                                                                                    {'x':[1,2,3,4,5],'y':[4,6,3,8,1],'type':'line','name':'Sugar'},],
+                                                                                   'layout':{'title':'Dash Example App'},
+                                                                                   }),
 
-@app.callback(
-    Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+dcc.Graph(id='dash_graph', figure = {'data':[{'x':[1,2,3,4,5],'y':[4,6,3,8,1],'type':'bar','name':'Bread'},
+                                                                                    {'x':[1,2,3,4,5],'y':[9,10,11,1],'type':'bar','name':'Aeroplane'},
+                                                                                    {'x':[1,2,3,4,5],'y':[4,6,3,8,1],'type':'line','name':'Car'},
+                                                                                    {'x':[1,2,3,4,5],'y':[4,6,3,8,1],'type':'line','name':'Train'},],
+                                                                                   'layout':{'title':'Dash Example App'}})])
